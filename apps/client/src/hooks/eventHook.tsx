5169f3589aa0159dbb759b814b76aea516abd399
doro-hd <doro-hd@protonmail.com>
2025-05-22 14:42:14 +0200

    Fix small inconsistancy

--- apps/client/src/hooks/eventHook.tsx
+++ apps/client/src/hooks/eventHook.tsx
@@ -1,20 +1,13 @@
 import EventClient from "@/apiClients/eventClient";
 import { queryData } from "./dataHook";
-import { useQuery } from "@tanstack/react-query";
 
 const QUERY_KEY = "events";
 const eventClient = new EventClient();
 
 function useEvent(eventId: string) {
-  /*
   const query = queryData([QUERY_KEY, eventId], async () =>
     eventClient.findEventById(eventId),
-    );*/
-
-  const query = useQuery({
-    queryKey: [QUERY_KEY, eventId],
-    queryFn: () => eventClient.findEventById(eventId),
-  });
+  );
 
   return query;
 }
