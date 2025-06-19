b4d364ed03b6a5d7b883475bf1a14f470ee1c248
doro-hd <sid.doro-hd@protonmail.com>
2025-06-19 15:01:53 +0200

    Change vite cors setting

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -6,6 +6,9 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: false,
+  },
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
